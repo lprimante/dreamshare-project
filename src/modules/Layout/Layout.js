0f6a28bc539aed43d4470412d66e8f3485917251
lprimante <lprimante@gmail.com>
2020-03-06 07:47:34 -0300

    feat: implement new footer

--- src/modules/Layout/Layout.js
+++ src/modules/Layout/Layout.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
 
 import GlobalStyles from '../../styles'
 import Header from '../Header'
+import Footer from '../Footer'
+
+import * as S from './Layout.styles'
 
 const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles />
-      <Header/>
-      <div>
-        <main>{children}</main>
-        <footer />
-      </div>
+      <Header />
+      <S.Wrapper>{children}</S.Wrapper>
+      <Footer />
     </>
   )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
